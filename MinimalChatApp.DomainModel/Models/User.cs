506604ddfc471105d5a42c872915b5d59b8c0594
Rahul <mrahulmaity623@gmail.com>
2024-09-05 13:33:00 +0530

    Add project files.

---
+++ MinimalChatApp.DomainModel/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalChatApp.DomainModel.Models;
+public class User
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string FullName { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
